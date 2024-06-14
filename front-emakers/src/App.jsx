2980659ede2c57695acb199080c19437b0808987
Tiago de Paula <brtiagomartins@gmail.com>
2024-06-14 19:37:44 -0300

    .

--- front-emakers/src/App.jsx
+++ front-emakers/src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
 import React from "react";
+<<<<<<< Updated upstream
 import Login from "./pages/Login/TelaLogin";
 
 function App() {
   return (
     <Login />
+=======
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TelaRegistro from "./pages/registro/TelaRegistro";
+import TelaHome from "./pages/home/TelaHome"; // Importe o componente da tela inicial
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TelaLogin />} />
+        <Route path="/registro" element={<TelaRegistro />} />
+        <Route path="/home" element={<TelaHome />} /> 
+      </Routes>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
---
+++ front-emakers/src/pages/login/TelaLogin.jsx
@@ -0,0 +1,49 @@
+// TelaLogin.js
+import "./login.css";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function TelaLogin() {
+  const navigate = useNavigate();
+
+  const handleRegistrarClick = () => {
+    navigate("/registro");
+  };
+
+  const handleEntrarClick = () => {
+    navigate("/home");
+  }
+
+return (
+  <div>
+    <div class="div-principal-tela-login">
+      <div class="div-esquerda-tela-login">
+        <img src={"src/img/brancologo.png"} alt="Logo Emakers" class="logo-emakers" draggable="false" />
+        <div class="div-esquerda-conteudo"></div>
+        <h1 class="entre">Entre em Sua Conta</h1>
+        <form action="" autocomplete="off" class="form-login">
+          <fieldset>
+            <label for="email" class="form-email">E-mail:</label>
+            <input type="email" name="email" />
+            <label for="password" class="form-password">Senha:</label>
+            <div class="password-container">
+              <input type="password" name="password" id="password" />
+              <i class="bi bi-eye-fill" id="btn-password" onclick="mostrarSenha()"></i>
+            </div>
+            <button type="submit" class="form-button-entrar" onClick={handleEntrarClick} >Entrar</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+    <div class="div-direita-tela-login">
+      <div class="div-direita-conteudo">
+        <h1>Novo Aqui?</h1>
+        <p>Registre-se e venha fazer parte da maior rede social!</p>
+        <button class="button-registrar" onClick={handleRegistrarClick}>Registrar-se</button>
+      </div>
+    </div>
+  </div>
+);
+}
+
+export default TelaLogin;
